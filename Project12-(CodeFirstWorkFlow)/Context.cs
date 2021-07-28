8c4fa8b2dc02328f2a1a85b64ecd8c1b189a462a
Chris Cooper <bloodwolf444@gmail.com>
2021-07-28 11:06:04 -0700

    Added the Dbcontext class to our project and linked it with the ComicBook entity

---
+++ Project12-(CodeFirstWorkFlow)/Context.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project12__CodeFirstWorkFlow_.Models;
+
+namespace Project12__CodeFirstWorkFlow_
+{
+    public class Context : DbContext
+    {
+        //this represents one entity in our project.
+        // all the entities would go in here.
+        public DbSet<ComicBook> comicBooks { get; set; }
+    }
+}
